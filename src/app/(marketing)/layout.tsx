86fb9f327ba3fe712cc859b6149bd274a010e3d0
Alvaro Sena <alvarosenacs.c@gmail.com>
2026-02-28 16:25:15 -0300

    feat: add waitlist page

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/footer";
+
+export default function MarketingLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div
+        id="home"
+        className="absolute inset-0 dark:bg-[linear-gradient(to_right,#000000_1px,transparent_1px),linear-gradient(to_bottom,#000000_1px,transparent_1px)] bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:3rem_3rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] h-full"
+      />
+      <main className="mt-20 mx-auto w-full z-0 relative">{children}</main>
+      <Footer />
+    </>
+  );
+}
